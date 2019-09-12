db55cedb8fa446cbe5b1ef8d9995eb94400c0444
Armando <aindraju@gmail.con>
2019-09-12 19:37:13 +0200

    setting up django

---
+++ rp-portfolio/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\armando\documents\gitprojects\django_tutorial\rp-portfolio\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
